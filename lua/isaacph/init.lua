3bf5594d58641c94644b20f31fbfa3a722322337
isaacph <isaacphuffman@gmail.com>
2023-07-05 13:22:05 -0400

    Debugging

--- lua/isaacph/init.lua
+++ lua/isaacph/init.lua
@@ -1,5 +1,7 @@
 require("isaacph.packer")
+print("OS ENV INITNVIM:", os.getenv("INITNVIM"))
 if not os.getenv("INITNVIM") then
+    print("calling other scripts")
     require("isaacph.remap")
     require("isaacph.set")
 end
