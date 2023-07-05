8d7de5b9516cbf2efe65fc76f6d2e161986e961c
isaacph <isaacphuffman@gmail.com>
2023-07-05 13:00:46 -0400

    Disable bootstrap

--- lua/isaacph/packer.lua
+++ lua/isaacph/packer.lua
@@ -1,17 +1,18 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
+-- for now we're just going to script outside of packer config
+-- local ensure_packer = function()
+--   local fn = vim.fn
+--   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+--   if fn.empty(fn.glob(install_path)) > 0 then
+--     fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+--     vim.cmd [[packadd packer.nvim]]
+--     return true
+--   end
+--   return false
+-- end
+-- 
+-- local packer_bootstrap = ensure_packer()
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -65,11 +66,4 @@ return require('packer').startup(function(use)
   }
 
   use { 'rush-rs/tree-sitter-asm' } -- for assemmbly tree sitter highlighting
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    print("Trying to sync")
-    require('packer').sync()
-  end
 end)
